a6956ec496f38b133e61688d2a6b82286f14d49d
shan <corallishan@gmail.com>
2013-10-22 16:28:12 -0400

    check if bananas are in the list

--- exercise6.rb
+++ exercise6.rb
@@ -1,4 +1,17 @@
 grocery_list = ["carrots", "toilet paper", "apples", "salmon"]
-grocery_list.each do |name|
-  puts "* #{name}"
+
+def print_list(grocery_list)
+  grocery_list.each do |name|
+    puts "* #{name}"
+  end
 end
+grocery_list << "rice"
+
+print_list(grocery_list)
+puts grocery_list.length
+if grocery_list.include?("banbanas")
+  puts"You need to pick up bananas."
+else
+  puts"You don't need to pick up banbanas today."
+end
+
