048d63e4dca8fe4d9fbd367a426e7126b579425a
shan <corallishan@gmail.com>
2013-10-22 14:54:02 -0400

    the second exercise

---
+++ exercise2.rb
@@ -0,0 +1,15 @@
+def tip(money)
+  tip= money*0.15
+  puts tip
+end
+tip(55)
+
+
+a=10
+puts "I have #{a} dallers!"
+
+a=45628
+b=7839
+puts "#{a}*#{b}= #{a*b}"
+
+puts (true&&false) || (false&&true) || !(false&&false)
